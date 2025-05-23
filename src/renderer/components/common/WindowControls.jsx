749dc56939440addc80b8be01de55903ee576fd6
elzuzu <45237180+elzuzu@users.noreply.github.com>
2025-05-23 18:40:12 +0200

    style: refine window controls

--- src/renderer/components/common/WindowControls.jsx
+++ src/renderer/components/common/WindowControls.jsx
@@ -5,13 +5,13 @@ export default function WindowControls() {
   return (
     <div className="window-controls">
       <button className="window-control minimize" onClick={() => window.api.minimizeWindow()}>
-        <Minus size={16} />
+        <Minus size={12} strokeWidth={3} />
       </button>
       <button className="window-control maximize" onClick={() => window.api.maximizeWindow()}>
-        <Square size={14} />
+        <Square size={10} strokeWidth={3} />
       </button>
       <button className="window-control close" onClick={() => window.api.closeWindow()}>
-        <X size={16} />
+        <X size={12} strokeWidth={3} />
       </button>
     </div>
   );
