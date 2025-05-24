01b9798f5a34ef090e68fe3789d1b4e0c711c900
elzuzu <45237180+elzuzu@users.noreply.github.com>
2025-05-24 10:27:02 +0200

    Delete src directory

--- src/renderer/store.ts
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand';
-
-export const useRows = create<{ rows: any[]; set: (r: any[]) => void }>((set) => ({
-  rows: [],
-  set: (rows) => set({ rows })
-}));
-
-window.api.on('db:refresh', (_, r) => useRows.getState().set(r));
